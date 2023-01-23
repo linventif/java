1b13be58c0703b261b443903b4141601e459b2fb
Linventif <linventif@gmail.com>
2023-01-23 15:49:34 +0100

    2023 01 23

---
+++ printHello.java
@@ -0,0 +1,12 @@
+// made a simple hello world program
+
+public class printHello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+// who to run this program
+// 1. open terminal
+// 2. javac printHello.java
+// 3. java printHello
