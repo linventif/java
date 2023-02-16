c084fa090fb29114e1b980f91cba00ef86d7e58b
Linventif <linventif@gmail.com>
2023-02-16 08:56:22 +0100

    2023 02 16

--- dev-oo/src/tp03/Card.java
+++ dev-oo/src/tp03/Card.java
@@ -8,13 +8,45 @@ public class Card {
         return color;
     }
 
+    public Rank getRank() {
+        return rank;
+    }
+
     public Card(Color color, Rank rank) {
         this.color = color;
         this.rank = rank;
     }
 
+    public Card(String color, String rank) {
+        this.color = Color.valueOf(color);
+        this.rank = Rank.valueOf(rank);
+    }
+
     public void setColor(Color color) {
         this.color = color;
+    }
+
+    public boolean compareColor(Card otherCard) {
+        return this.color == otherCard.color;
+    }
+
+    public boolean compareRank(Card otherCard) {
+        return this.rank == otherCard.rank;
+    }
+
+    public Card isBefore(Card otherCard) {
+        if (this.rank.ordinal() < otherCard.rank.ordinal()) {
+            return this;
+        }
+        return otherCard;
+    }
+
+    public boolean equals(Object otherCard) {
+        if (otherCard instanceof Card) {
+            Card other = (Card) otherCard;
+            return compareColor(other) && compareRank(other);
+        }
+        return false;
     }
 
     public String toString() {
